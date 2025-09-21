fd801a32b79dba9487e9e8ba70ffd69e0582e885
CyzerXd <100297681+CyXd404@users.noreply.github.com>
2025-09-21 18:14:55 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Projects from './components/Projects';
 import Education from './components/Education';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import NotFound from "./pages/NotFound";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
